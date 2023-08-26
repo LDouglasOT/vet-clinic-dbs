77c88376a3c3c9fd9850f2875d9889df6726fa7f
ActiveApps <douglas.luzinda.ot@gmail.com>
2023-08-26 10:42:47 +0300

    query multiple tables

--- schema.sql
+++ schema.sql
@@ -14,3 +14,30 @@ CREATE TABLE animals (
 /* Create column called species */
 ALTER TABLE animals
 ADD species VARCHAR(255);
+
+
+psql
+/c mydatabases
+CREATE TABLE owner (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INTEGER
+);
+
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+SELECT * FROM animals;
+
+ALTER TABLE animals
+DROP COLUMN species;
+SELECT * FROM animals;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER REFERENCES owner(id);
+SELECT * FROM animals;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER REFERENCES owner(id);
+SELECT * FROM animals;
